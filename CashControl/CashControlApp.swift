fc904c2c5a8e73e15d4d90610524085307d108ca
Joseph DeWeese <jdeweese@weldtascics.systems>
2024-03-16 19:50:38 -0500

    Initial Commit

---
+++ CashControl/CashControlApp.swift
@@ -0,0 +1,17 @@
+//
+//  CashControlApp.swift
+//  CashControl
+//
+//  Created by J. DeWeese on 3/16/24.
+//
+
+import SwiftUI
+
+@main
+struct CashControlApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
